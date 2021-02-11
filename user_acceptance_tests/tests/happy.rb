ff65f52b850e4980abb867ce16b92cdd1901c765
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-02-11 10:40:14 -0800

    [DEMO-2557] Fix test endpoints, add in retry
    * Change the endpoints to actually test the endpoints we want.
    * Added in retry gem so that we can wait for the api to be functional
    before we start testing. In practice, it looks like the tests are
    running while the api is starting up, so giving the tests a way to wait
    for the api.

--- user_acceptance_tests/tests/happy.rb
+++ user_acceptance_tests/tests/happy.rb
@@ -2,6 +2,7 @@ require 'minitest/spec'
 require 'minitest/autorun'
 require 'rest-client'
 require 'json'
+require 'retriable'
 
 describe 'Deployment Tests' do
   let(:service_url) do
@@ -10,23 +11,32 @@ describe 'Deployment Tests' do
     )
   end
 
+  before do
+    self.class.wait_for_api(service_url)
+  end
+
   it 'GET /api/inventory should return HTTP 200 OK' do
     response = RestClient.get("#{service_url}/api/inventory")
     expect(response.code).must_equal(200)
   end
 
   it 'GET /api/inventory/1 should return HTTP 200 OK' do
-    response = RestClient.get("#{service_url}/api/inventory")
+    response = RestClient.get("#{service_url}/api/inventory/1")
     expect(response.code).must_equal(200)
   end
 
   it 'GET /api/validateMessage should return HTTP 200 OK' do
-    response = RestClient.get("#{service_url}/api/inventory")
+    response = RestClient.get("#{service_url}/api/validateMessage")
     expect(response.code).must_equal(200)
   end
 
   it 'GET /api/behaviors should return HTTP 200 OK' do
-    response = RestClient.get("#{service_url}/api/inventory")
+    response = RestClient.get("#{service_url}/api/behaviors")
+    expect(response.code).must_equal(200)
+  end
+
+  it 'GET /api/help should return HTTP 200 OK' do
+    response = RestClient.get("#{service_url}/api/help")
     expect(response.code).must_equal(200)
   end
 
@@ -59,6 +69,18 @@ describe 'Deployment Tests' do
       exit(1)
     else
       service_url
+    end
+  end
+
+  def self.wait_for_api(service_url)
+    if @before_flag.nil?
+      @before_flag = true
+
+      # this will retry 5 additional times after the initial attempt, at each interval represented in seconds. maximum total wait time is 16 seconds.
+      Retriable.retriable(intervals: [1, 2, 4, 8, 16]) do
+        response = RestClient.get("#{service_url}/api/inventory")
+        raise Exception unless response.code == 200
+      end
     end
   end
 end

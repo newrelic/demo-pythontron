aec6b8c2539d3b97276fbcc335c4899a73b6bdb5
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-02-12 15:48:43 -0800

    [DEMO-2557] Copy over workflow & needed files (#6)
    * [DEMO-2557] Copy over workflow & needed files
    
    * [DEMO-2557] Add github run id to user config name
    
    * This lets multiple invocations of the workflow run without conflicting
    with each other -- the resources will have different names since the run
    ids will be different.
    
    * [DEMO-2557] Fix test endpoints, add in retry
    
    * Change the endpoints to actually test the endpoints we want.
    * Added in retry gem so that we can wait for the api to be functional
    before we start testing. In practice, it looks like the tests are
    running while the api is starting up, so giving the tests a way to wait
    for the api.
    
    * [DEMO-2557] Update main README with link to UAT documentation
    
    * [DEMO-2557] Add lambda scenario
    
    * Adding in the lambda scenario, deployment, and tests.
    
    * [DEMO-2557] Shorten user credential name
    
    * Deployment was failing to name length limit being exceeded, so removed
    part of the name to get the length down.
    
    * [DEMO-2557] Modify workflow to be able to skip matrix combinations
    
    * Certain combinations are currently not possible. Example: deployment
    of lambda to azure. The workflow is currently expecting all possible
    combinations to be executable, so this commit adds in a way for the
    workflow to skip this, by first checking if a particular combination has
    a deployment config or not.
    
    * [DEMO-2557] Remove behaviors endpoint test
    
    * Removing behaviors endpoint test. When deployed as a lambda, this
    endpoint is not supported.

---
+++ user_acceptance_tests/tests/happy.rb
@@ -0,0 +1,86 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'rest-client'
+require 'json'
+require 'retriable'
+
+describe 'Deployment Tests' do
+  let(:service_url) do
+    get_service_url_json(
+      get_test_input(ENV['TEST_INPUT_FILE_LOCATION'])
+    )
+  end
+
+  before do
+    self.class.wait_for_api(service_url)
+  end
+
+  it 'GET /api/inventory should return HTTP 200 OK' do
+    response = RestClient.get("#{service_url}/api/inventory")
+    expect(response.code).must_equal(200)
+  end
+
+  it 'GET /api/inventory/1 should return HTTP 200 OK' do
+    response = RestClient.get("#{service_url}/api/inventory/1")
+    expect(response.code).must_equal(200)
+  end
+
+  it 'GET /api/validateMessage should return HTTP 200 OK' do
+    response = RestClient.get("#{service_url}/api/validateMessage")
+    expect(response.code).must_equal(200)
+  end
+
+  it 'GET /api/behaviors should return HTTP 200 OK' do
+    response = RestClient.get("#{service_url}/api/behaviors")
+    expect(response.code).must_equal(200)
+  end
+
+  it 'GET /api/help should return HTTP 200 OK' do
+    response = RestClient.get("#{service_url}/api/help")
+    expect(response.code).must_equal(200)
+  end
+
+  def get_test_input(file_path)
+    test_input = File.read(file_path)
+    test_input
+  end
+
+  def get_service_url_json(test_input)
+    service_url = JSON.parse(test_input)
+                      .fetch('services', [{}])
+                      .first
+                      .fetch('urls', [])
+                      .first
+    if service_url.nil?
+      puts 'Test input does not contain a url for testing'
+      puts "Test input: \n#{test_input}"
+      exit(1)
+    else
+      service_url
+    end
+  end
+
+  def get_service_url_regex(test_input)
+    service_url = test_input.scan(/testing_endpoint:'(.+)'/)[0][0]
+
+    if service_url.nil?
+      puts 'Test input does not contain a url for testing'
+      puts "Test input: \n#{test_input}"
+      exit(1)
+    else
+      service_url
+    end
+  end
+
+  def self.wait_for_api(service_url)
+    if @before_flag.nil?
+      @before_flag = true
+
+      # this will retry 5 additional times after the initial attempt, at each interval represented in seconds. maximum total wait time is 16 seconds.
+      Retriable.retriable(intervals: [1, 2, 4, 8, 16]) do
+        response = RestClient.get("#{service_url}/api/inventory")
+        raise Exception unless response.code == 200
+      end
+    end
+  end
+end

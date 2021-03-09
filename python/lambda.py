a04dc59edd1216609ad4e9b49fc1b7d0894aa7fe
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-03-09 13:44:37 -0800

    [DEMO-2561] Add SimpleContainer, use in lambda entrypoint
    * Created SimpleContainer, an attempt at copying dependency_injector's
    Container class, for use in the lambda entrypoint. This is because the
    dependency_injector module is creating runtime errors when used (due
    to some special setup needed for installing it).
    * Removed unnecessary usings.

--- python/lambda.py
+++ python/lambda.py
@@ -1,16 +1,8 @@
-from api.index_handler import IndexHandler
 from api.help import help_message
-from api.message_handler import MessageHandler
-from lib.datastore import DataStore
-from api.inventory_handler import InventoryHandler
-from lib.app_config import AppConfig
-from lib.validate.configuration import Configuration
-from lib.http_utils import HttpUtils
-from lib.tron_response import TronResponse
 from lib.app_logging import AppLogging
-from lib.behaviors import repository
+from lib.tron_response import TronResponse
+from simple_container import SimpleContainer
 
-import os
 
 def get_lambda_reponse(tron_response):
     return {
@@ -21,28 +13,23 @@ def get_lambda_reponse(tron_response):
 
 def lambda_handler(event, context):
     AppLogging.init('info')
-    config_file = os.environ['config_file']
-    Configuration(config_file).validate_config()
-    app_config = AppConfig(config_file)
-
-    func_get_headers = lambda: (event['headers'] or dict()).items()
-    http_utils = HttpUtils(func_get_headers)
-    behavior_repository = repository.Repository(app_id=app_config.get_app_id())
+    
+    container = SimpleContainer()
 
     endpoint = (event['resource'] or "").lower()
     if endpoint == "/":
-        index = IndexHandler(app_config, behavior_repository)
+        index = container.index_handler
         resp =  index.index_message()
     elif endpoint == "/validatemessage":
-        message = MessageHandler(app_config, http_utils, behavior_repository)
+        message = container.message_handler
         resp =  message.validate()
     elif endpoint == "/help":
         resp = help_message()
     elif endpoint == "/inventory":
-        inventory = InventoryHandler(app_config, DataStore(), http_utils, behavior_repository)
+        inventory = container.inventory_handler
         resp = inventory.get_inventory()
     elif endpoint == "/inventory/{id+}":
-        inventory = InventoryHandler(app_config, DataStore(), http_utils, behavior_repository)
+        inventory = container.inventory_handler
         item_id = event['pathParameters']['id']
         resp = inventory.get_inventory_item(item_id)
     else:

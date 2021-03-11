dcf5c9030fe89595b9ed92793bd4691a89f23e90
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-03-11 15:13:33 -0800

    [DEMO-2564] Add method to run invalid query

--- python/repository/database_inventory_repository.py
+++ python/repository/database_inventory_repository.py
@@ -4,11 +4,12 @@ from repository.i_database_connector import IDatabaseConnector
 from repository.i_inventory_repository import IInventoryRepository
 
 
-class DatabaseInventoryRepository(IInventoryRepository):
+class DatabaseInventoryRepository(IInventoryRepository):    
     def __init__(self, database_connector: IDatabaseConnector) -> None:
         self.__database_connector = database_connector
 
     def get_records(self):
+        # dictionary=True is to return output as a dictionary, rather than multi dimensional array
         with self.__database_connector.connect().cursor(dictionary=True) as cursor:
             query = ("SELECT * FROM inventory")
             cursor.execute(query)
@@ -21,3 +22,9 @@ class DatabaseInventoryRepository(IInventoryRepository):
             cursor.execute(query, (id,))
             items = [i for i in cursor]
             return items[0] if len(items) == 1 else None
+        
+    def run_invalid_query(self):
+        with self.__database_connector.connect() as cursor:
+            query = ("SELECT * FROM inventry")
+            cursor.execute(query)
+            return [i for i in cursor]
